b02cc2d1d318cbc16ef022d486656afad85d15c5
GaraGuruTak <garagurutak@gmail.com>
2023-05-10 13:35:38 -0600

    Practica 1 del curso Fullstack Open 2023

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//const App = () => {
+//  const now = new Date()
+//  const a = 10
+//  const b = 20
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//  return (
+//    <div>
+//      <p>Hello world, it is {now.toString()}</p>
+//      <p>
+//        {a} plus {b} is {a + b}
+//      </p>
+//    </div>
+//  )
+//}
+
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name} you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>  
+      </h1>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter CONST'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello name="George" />
+      <Hello name="Daisy" />
+      <Hello name="Kevin Flores"  age={27 + 10}/>
+      <Hello name="Akuma Takeshi" />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root')) 
